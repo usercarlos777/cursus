7a08d176a4618e31c25c3776a80d56fbbe9a0364
Steven Orrego <stevenorrego@leadercentersas>
2021-08-31 15:19:24 -0500

    static_asset

--- resources/views/inc/scripts.blade.php
+++ resources/views/inc/scripts.blade.php
@@ -3,17 +3,17 @@
 </script>
 <script src="{{ static_asset('/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ static_asset('/admin/assets/js/nicescroll.min.js')}}"></script>
-<script src="{{ static_asset('assets/js/moment.min.js')}}"></script>
+<script src="{{ static_asset('/admin/assets/js/moment.min.js')}}"></script>
 
 <script src="{{ static_asset('/admin/assets/plugins/table/datatable/datatables.js')}}"></script>
 <script src="{{ static_asset('/admin/assets/plugins/table/datatable/button-ext/dataTables.buttons.min.js')}}"></script>
 <script src="{{ static_asset('/admin/assets/plugins/table/datatable/button-ext/jszip.min.js')}}"></script>
 <script src="{{ static_asset('/admin/assets/plugins/table/datatable/button-ext/buttons.html5.min.js')}}"></script>
-<script src="{{ static_asset('assets/plugins/table/datatable/button-ext/buttons.print.min.js')}}"></script>
-<script src="{{ static_asset('assets/js/stisla.js') }}"></script>
-<script src="{{ static_asset('assets/plugins/select2/select2.min.js')}}"></script>
-<script src="{{ static_asset('assets/js/scripts.js') }}"></script>
-<script src="{{ static_asset('assets/js/custom.js') }}"></script>
-<script src="{{ static_asset('assets/js/chart.js') }}"></script>
-<link href="{{ static_asset('assets/js/summernote.min.css') }}" rel="stylesheet">
-<script src="{{ static_asset('assets/js/summernote.min.js') }}"></script>
+<script src="{{ static_asset('/admin/assets/plugins/table/datatable/button-ext/buttons.print.min.js')}}"></script>
+<script src="{{ static_asset('/admin/assets/js/stisla.js') }}"></script>
+<script src="{{ static_asset('/admin/assets/plugins/select2/select2.min.js')}}"></script>
+<script src="{{ static_asset('/admin/assets/js/scripts.js') }}"></script>
+<script src="{{ static_asset('/admin/assets/js/custom.js') }}"></script>
+<script src="{{ static_asset('/admin/assets/js/chart.js') }}"></script>
+<link href="{{ static_asset('/admin/assets/js/summernote.min.css') }}" rel="stylesheet">
+<script src="{{ static_asset('/admin/assets/js/summernote.min.js') }}"></script>
